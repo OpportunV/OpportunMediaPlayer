923c96086e5319506dc4966008b2dc4e85c731e4
OpportunV <RsTGear@gmail.com>
2026-08-09 13:31:24 +0300

    Moved to PortAudio, verified on windows

---
+++ OMP.Lib/Audio/Output/IAudioOutput.cs
@@ -0,0 +1,14 @@
+using NAudio.Wave;
+
+namespace OMP.Lib.Audio.Output;
+
+internal interface IAudioOutput : IDisposable
+{
+    public int PreferredSampleRate { get; }
+
+    public void Init(IWaveProvider source);
+
+    public void Play();
+
+    public void Pause();
+}
